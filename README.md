5cd5993422c73897e6e2f01c708531aa6d0a6a25
daniel-mcclintock <13973653+daniel-mcclintock@users.noreply.github.com>
2021-11-09 21:43:36 +1100

    Add Daniel+ and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # danielplus
+## death to disney+
+
+```sh
+# Fzf a movie to watch
+movie
+
+# Fzf a series to watch
+series
+
+# Watch a random movie
+random-movie
+
+# Watch a random series
+random-series
+
+# Infinitely watch random media
+daniel+
+```
+
+ - Assumes movies are stored in individial directories
+   - eg: /media/path/{movie_directory}/{movie_file}.mkv
+ - Assumes series are stored in individial directories, with season sub-directories
+   - eg: /media/path/{series_directory}/{series_season}/{series_s01e01}.mkv
+
+ - depends gsudo, fzf, mpv, cifs-utils
+ - export `CIFS_MEDIA_REMOTE_PATH` with your CIFS/Samba Network path to your "legit" media server.
+   eg: `export CIFS_MEDIA_REMOTE_PATH=//192.168.1.2/movies_and_shit`
+ - export `CIFS_MEDIA_LOCAL_PATH` with a local path to mount media to
+ - eg: `export CIFS_MEDIA_LOCAL_PATH=/mnt/movies_and_shit`
+ - source danielplus.sh
---
+++ danielplus.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+media() {
+    gsudo mount -t cifs $CIFS_MEDIA_REMOTE_PATH $CIFS_MEDIA_LOCAL_PATH -o user=$USER && cd $CIFS_MEDIA_LOCAL_PATH
+}
+
+movie() {
+    IS_MOUNTED="$(mount | grep $CIFS_MEDIA_REMOTE_PATH | wc -l)"
+
+    if [ "$IS_MOUNTED" -eq "1" ]; then
+        cd $CIFS_MEDIA_LOCAL_PATH/movies >>/dev/null
+        MOVIE="$(ls | fzf)"
+        echo "$MOVIE"
+        mpv "./$MOVIE"
+    else
+        media
+        movie
+    fi
+}
+
+series() {
+    IS_MOUNTED="$(mount | grep $CIFS_MEDIA_REMOTE_PATH | wc -l)"
+
+    if [ "$IS_MOUNTED" -eq "1" ]; then
+        cd $CIFS_MEDIA_LOCAL_PATH/series >>/dev/null
+        SERIES="$(ls | fzf)"
+        echo "$SERIES"
+
+        SEASON="all $(ls "$SERIES/")"
+        SEASON="$(echo $SEASON | tr " " "\n" | fzf)"
+
+        if [ "$SEASON" == "all" ]; then
+            mpv "./$SERIES/"
+        else
+            EPISODE="all $(ls "./$SERIES/$SEASON/")"
+            EPISODE="$(echo $EPISODE | tr " " "\n" | fzf)"
+
+            if [ "$EPISODE" == "all" ]; then
+                mpv "./$SERIES/$SEASON/"
+            else
+                mpv "./$SERIES/$SEASON/$EPISODE"
+            fi
+        fi
+    else
+        media
+        movie
+    fi
+}
+
+random-series() {
+    IS_MOUNTED="$(mount | grep $CIFS_MEDIA_REMOTE_PATH | wc -l)"
+
+    if [ "$IS_MOUNTED" -eq "1" ]; then
+        cd $CIFS_MEDIA_LOCAL_PATH/series >>/dev/null
+        SERIES="$(ls | sort -R | head -n 1)"
+        echo "$SERIES"
+        mpv "$SERIES" --no-terminal
+    else
+        media
+        random-series
+    fi
+}
+
+random-movie() {
+    IS_MOUNTED="$(mount | grep $CIFS_MEDIA_REMOTE_PATH | wc -l)"
+
+    if [ "$IS_MOUNTED" -eq "1" ]; then
+        cd $CIFS_MEDIA_LOCAL_PATH/movies >>/dev/null
+        MOVIE="$(ls | sort -R | head -n 1)"
+        echo "$MOVIE"
+        mpv "$MOVIE" --no-terminal
+    else
+        media
+        random-movie
+    fi
+}
+
+daniel-plus() {
+    while [ true ]; do
+      if [ "$(($RANDOM % 10))" -lt 5 ]; then
+        printf "Movie: "
+        random-movie
+      else
+        printf "Series: "
+        random-series
+      fi
+    done
+}
+alias daniel+=daniel-plus
