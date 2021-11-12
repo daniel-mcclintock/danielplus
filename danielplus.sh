bf5201547f7fd4c364bc42d2f0b50e8c9a417683
daniel-mcclintock <13973653+daniel-mcclintock@users.noreply.github.com>
2021-11-12 15:59:25 +1100

    Fix series() when media isn't mounted
     - add recent-movie() for picking recently added movies
     - shfmt

--- danielplus.sh
+++ danielplus.sh
@@ -1,89 +1,104 @@
 #!/bin/bash
 
 media() {
-    gsudo mount -t cifs $CIFS_MEDIA_REMOTE_PATH $CIFS_MEDIA_LOCAL_PATH -o user=$USER && cd $CIFS_MEDIA_LOCAL_PATH
+  gsudo mount -t cifs $CIFS_MEDIA_REMOTE_PATH $CIFS_MEDIA_LOCAL_PATH -o user=$USER && cd $CIFS_MEDIA_LOCAL_PATH
 }
 
 movie() {
-    IS_MOUNTED="$(mount | grep $CIFS_MEDIA_REMOTE_PATH | wc -l)"
+  IS_MOUNTED="$(mount | grep $CIFS_MEDIA_REMOTE_PATH | wc -l)"
 
-    if [ "$IS_MOUNTED" -eq "1" ]; then
-        cd $CIFS_MEDIA_LOCAL_PATH/movies >>/dev/null
-        MOVIE="$(ls | fzf)"
-        echo "$MOVIE"
-        mpv "./$MOVIE"
-    else
-        media
-        movie
-    fi
+  if [ "$IS_MOUNTED" -eq "1" ]; then
+    cd $CIFS_MEDIA_LOCAL_PATH/movies >>/dev/null
+    MOVIE="$(ls | fzf)"
+    echo "$MOVIE"
+    mpv "./$MOVIE"
+  else
+    media
+    movie
+  fi
 }
 
 series() {
-    IS_MOUNTED="$(mount | grep $CIFS_MEDIA_REMOTE_PATH | wc -l)"
+  IS_MOUNTED="$(mount | grep $CIFS_MEDIA_REMOTE_PATH | wc -l)"
 
-    if [ "$IS_MOUNTED" -eq "1" ]; then
-        cd $CIFS_MEDIA_LOCAL_PATH/series >>/dev/null
-        SERIES="$(ls | fzf)"
-        echo "$SERIES"
+  if [ "$IS_MOUNTED" -eq "1" ]; then
+    cd $CIFS_MEDIA_LOCAL_PATH/series >>/dev/null
+    SERIES="$(ls | fzf)"
+    echo "$SERIES"
 
-        SEASON="all $(ls "$SERIES/")"
-        SEASON="$(echo $SEASON | tr " " "\n" | fzf)"
+    SEASON="all $(ls "$SERIES/")"
+    SEASON="$(echo $SEASON | tr " " "\n" | fzf)"
 
-        if [ "$SEASON" == "all" ]; then
-            mpv "./$SERIES/"
-        else
-            EPISODE="all $(ls "./$SERIES/$SEASON/")"
-            EPISODE="$(echo $EPISODE | tr " " "\n" | fzf)"
-
-            if [ "$EPISODE" == "all" ]; then
-                mpv "./$SERIES/$SEASON/"
-            else
-                mpv "./$SERIES/$SEASON/$EPISODE"
-            fi
-        fi
+    if [ "$SEASON" == "all" ]; then
+      mpv "./$SERIES/"
     else
-        media
-        movie
+      EPISODE="all $(ls "./$SERIES/$SEASON/")"
+      EPISODE="$(echo $EPISODE | tr " " "\n" | fzf)"
+
+      if [ "$EPISODE" == "all" ]; then
+        mpv "./$SERIES/$SEASON/"
+      else
+        mpv "./$SERIES/$SEASON/$EPISODE"
+      fi
     fi
+  else
+    media
+    series
+  fi
 }
 
 random-series() {
-    IS_MOUNTED="$(mount | grep $CIFS_MEDIA_REMOTE_PATH | wc -l)"
+  IS_MOUNTED="$(mount | grep $CIFS_MEDIA_REMOTE_PATH | wc -l)"
 
-    if [ "$IS_MOUNTED" -eq "1" ]; then
-        cd $CIFS_MEDIA_LOCAL_PATH/series >>/dev/null
-        SERIES="$(ls | sort -R | head -n 1)"
-        echo "$SERIES"
-        mpv "$SERIES" --no-terminal
-    else
-        media
-        random-series
-    fi
+  if [ "$IS_MOUNTED" -eq "1" ]; then
+    cd $CIFS_MEDIA_LOCAL_PATH/series >>/dev/null
+    SERIES="$(ls | sort -R | head -n 1)"
+    echo "$SERIES"
+    mpv "$SERIES" --no-terminal
+  else
+    media
+    random-series
+  fi
+}
+
+recent-movie() {
+  # The same as movie, sorted by directory creation date
+  IS_MOUNTED="$(mount | grep $CIFS_MEDIA_REMOTE_PATH | wc -l)"
+
+  if [ "$IS_MOUNTED" -eq "1" ]; then
+    cd $CIFS_MEDIA_LOCAL_PATH/movies >>/dev/null
+    MOVIE="$(ls -t | fzf)"
+    echo "$MOVIE"
+    mpv "./$MOVIE"
+  else
+    media
+    recent-movie
+  fi
 }
 
 random-movie() {
-    IS_MOUNTED="$(mount | grep $CIFS_MEDIA_REMOTE_PATH | wc -l)"
+  IS_MOUNTED="$(mount | grep $CIFS_MEDIA_REMOTE_PATH | wc -l)"
 
-    if [ "$IS_MOUNTED" -eq "1" ]; then
-        cd $CIFS_MEDIA_LOCAL_PATH/movies >>/dev/null
-        MOVIE="$(ls | sort -R | head -n 1)"
-        echo "$MOVIE"
-        mpv "$MOVIE" --no-terminal
-    else
-        media
-        random-movie
-    fi
+  if [ "$IS_MOUNTED" -eq "1" ]; then
+    cd $CIFS_MEDIA_LOCAL_PATH/movies >>/dev/null
+    MOVIE="$(ls | sort -R | head -n 1)"
+    echo "$MOVIE"
+    mpv "$MOVIE" --no-terminal
+  else
+    media
+    random-movie
+  fi
 }
 
 daniel-plus() {
-    while [ true ]; do
-      if [ "$(($RANDOM % 10))" -lt 5 ]; then
-        printf "Movie: "
-        random-movie
-      else
-        printf "Series: "
-        random-series
-      fi
-    done
+  while [ true ]; do
+    if [ "$(($RANDOM % 10))" -lt 5 ]; then
+      printf "Movie: "
+      random-movie
+    else
+      printf "Series: "
+      random-series
+    fi
+  done
 }
 alias daniel+=daniel-plus
